ae7db2cb55d802625bd8fe0e5b8cc3dac155e6ad
peng.xing <southeast_px@163.com>
2019-01-17 11:22:13 +0800

    准备javabean以及数据库表！

---
+++ src/cn/ksdshpx/mybatis/beans/Employee.java
@@ -0,0 +1,50 @@
+package cn.ksdshpx.mybatis.beans;
+
+/**
+ * @author peng.x
+ * @date 2019年1月17日 上午11:21:03
+ */
+public class Employee {
+	private Integer id;
+	private String lastName;
+	private String email;
+	private Integer gender;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getGender() {
+		return gender;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", lastName=" + lastName + ", email=" + email + ", gender=" + gender + "]";
+	}
+
+}
