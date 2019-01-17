f69c7ca3221c63adc1162b4eeeb81c022e25f81a
peng.xing <southeast_px@163.com>
2019-01-17 13:58:15 +0800

    HelloWorld 查询一个对象!

--- src/cn/ksdshpx/mybatis/test/TestMybatis.java
+++ src/cn/ksdshpx/mybatis/test/TestMybatis.java
@@ -8,6 +8,8 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
+import cn.ksdshpx.mybatis.beans.Employee;
+
 /**
  * @author peng.x
  * @date 2019年1月17日 下午12:26:23
@@ -22,6 +24,16 @@ public class TestMybatis {
 		System.out.println(sqlSessionFactory);
 		SqlSession sqlSession = sqlSessionFactory.openSession();
 		System.out.println(sqlSession);
+		try {
+			/**
+			 * statement:SQL语句的唯一标识
+			 * parameter:执行SQL用到的参数
+			 */
+			Employee employee = sqlSession.selectOne("org.mybatis.suibian.EmployeeMapper.selectEmployee", 1);
+			System.out.println(employee);
+		} finally {
+			sqlSession.close();
+		}
 	}
 
 }
