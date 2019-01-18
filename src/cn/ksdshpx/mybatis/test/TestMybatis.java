3302877b3f3a5037abf26347805b1da752dcf72a
peng.xing <southeast_px@163.com>
2019-01-18 15:17:15 +0800

    Mapper接口开发的好处！

--- src/cn/ksdshpx/mybatis/test/TestMybatis.java
+++ src/cn/ksdshpx/mybatis/test/TestMybatis.java
@@ -60,6 +60,11 @@ public class TestMybatis {
 			 * 两个绑定：
 			 * 		1.Mapper接口与SQL映射文件的绑定===>映射文件的namespace值必须指定为Mapper接口的全类名
 			 * 		2.Mapper接口的方法与SQL映射文件的具体SQL语句的绑定===>SQL语句的id值必须指定为Mpper接口的方法名
+			 * 
+			 * Mapper接口开发：
+			 * 		1.有更明确的类型
+			 * 		2.接口本身:接口本身就是抽象，抽出了规范
+			 * 			EmployeeDao:EmployeeDaoJdbcImpl、EmployeeDao:EmployeeDaoHibernateImpl、MyBatis代理实现类
 			 */
 			//获取MyBatis为Mapper接口生成的代理实现类对象
 			EmployeeDao dao = sqlSession.getMapper(EmployeeDao.class);
